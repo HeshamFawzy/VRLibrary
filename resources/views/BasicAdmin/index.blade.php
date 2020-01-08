6c4ff4c2ba4bb18b68f2225f74c78037d1785cc5
HeshamFawzy <46792902+HeshamFawzy@users.noreply.github.com>
2020-01-08 11:31:44 +0200

    p5

--- resources/views/BasicAdmin/index.blade.php
+++ resources/views/BasicAdmin/index.blade.php
@@ -2,13 +2,20 @@
 
 @section('content')
 	<div class="container d-flex justify-content-center">
-		<div class="card" style="width: 18rem;">
-		  <img class="card-img-top" src="https://via.placeholder.com/100" alt="Card image cap">
-		  <div class="card-body">
-		    <h5 class="card-title">Card title</h5>
-		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-		    <a href="#" class="btn btn-primary">Go somewhere</a>
-		  </div>
-		</div>
+		<div class="card p-2">
+		    <div class="row ">
+		        <div class="col-md-8 px-3">
+		            <div class="card-block px-3">
+		              <h4 class="card-title">Lorem ipsum dolor sit amet</h4>
+		              <p class="card-text">Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+		              <p class="card-text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		              <a href="#" class="btn btn-primary">Read More</a>
+		            </div>
+		        </div>
+		        <div class="col-md-4">
+		            <img src="https://via.placeholder.com/150" class="w-100">
+		        </div>
+		    </div>
+      	</div>
 	</div>
 @endsection
