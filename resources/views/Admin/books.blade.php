3f7ce4d2eeabec8075ddb0f98ddfe737e147aa0f
HeshamFawzy <46792902+HeshamFawzy@users.noreply.github.com>
2020-01-13 18:31:52 +0200

    p62

--- resources/views/Admin/books.blade.php
+++ resources/views/Admin/books.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="row p-1 m-2 d-flex justify-content-center">
 	<div class="col-12 p-2" style="background-color: white;">
+  <a href="{{ route('admin.index')}}" class="btn btn-primary">Back</a>
 	<table class="table table-hover">
   <thead>
     <tr>
